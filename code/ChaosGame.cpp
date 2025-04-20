9cfc49f75676f2ef784691828491c6846aab9bfc
Ahkar Min Hein <ahkarminhein@gmail.com>
2025-04-20 16:15:32 -0700

    Fourth Click Updated

--- code/ChaosGame.cpp
+++ code/ChaosGame.cpp
@@ -36,22 +36,23 @@ int main()
 		    }
 		    if (event.type == sf::Event::MouseButtonPressed)
 		    {
-			if (event.mouseButton.button == sf::Mouse::Left)
-			{
-			    std::cout << "the left button was pressed" << std::endl;
-			    std::cout << "mouse x: " << event.mouseButton.x << std::endl;
-			    std::cout << "mouse y: " << event.mouseButton.y << std::endl;
-	
-			    if(vertices.size() < 3)
-			    {
-				vertices.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
-			    }
-			    else if(points.size() == 0)
-			    {
-				///fourth click
-				///push back to points vector
-			    }
-			}
+				if (event.mouseButton.button == sf::Mouse::Left)
+				{
+					std::cout << "the left button was pressed" << std::endl;
+					std::cout << "mouse x: " << event.mouseButton.x << std::endl;
+					std::cout << "mouse y: " << event.mouseButton.y << std::endl;
+		
+					if(vertices.size() < 3)
+					{
+						vertices.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
+					}
+					else if(points.size() == 0)
+					{
+					///fourth click
+					///push back to points vector
+						points.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
+					}
+				}
 		    }
 		}
 		if (Keyboard::isKeyPressed(Keyboard::Escape))
